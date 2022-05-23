535d5adc3c411d6194dd4ec3142bfb075c090831
DanielD0 <37585619+DanielD0@users.noreply.github.com>
2022-05-23 10:08:44 -0500

    agregar iconos regulares en font-awesome

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import './index.css'
 import Notification from '@kyvg/vue3-notification'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
 
 
-library.add(fas);
+library.add(fas,far,fab);
 createApp(App)
 .use(router)
 .use(Notification)
