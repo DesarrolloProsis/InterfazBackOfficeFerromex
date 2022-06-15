05eb20ae8c3ee2a345e04f3b0ca1eae63c66c26a
CarlosPulidoMejia <78100114+CarlosPulidoMejia@users.noreply.github.com>
2022-06-15 18:00:15 -0500

    Documentacion Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,17 +6,17 @@
       <div class="absolute justify-end">
         <img class="img-centered rounded-full " src="~@/assets/Login/logoferromex.png" />
         <div class="bg-login-module h-96 " >
-          <div class="error" v-if="message != ''">{{message}}</div>
+          <div class="error" v-if="message != ''">{{message}}</div><!-- Div que contiene el mensaje de error -->
           <div class="input-container">
             <fa icon="circle-user" class="w-10 h-8 mt-1 mr-2 text-red-700"/>
             <input id="username" v-model="user" class="input-field" type="text" placeholder="Usuario" />
           </div>
           <div class="input-container">
             <fa icon="key" class="w-10 h-6 mt-3 mr-2 text-red-700"/>
-            <input id="password" v-model="pass" class="input-field" :type="tipoInput" placeholder="Contraseña" />
-              <span @click="tipoInput == 'password' ? tipoInput = 'text' : tipoInput = 'password'" class="absolute right-0 mt-3 mr-14 cursor-pointer">
-                <fa v-if="tipoInput == 'password'" icon="eye" class="text-gray-600 w-5 h-5" />
-                <fa v-else  icon="eye-slash" class="text-gray-600 w-5 h-5" />
+            <input id="password" v-model="pass" class="input-field" :type="tipoInput" placeholder="Contraseña" /><!-- input que contiene la contraseña -->
+              <span @click="tipoInput == 'password' ? tipoInput = 'text' : tipoInput = 'password'" class="absolute right-0 mt-3 mr-14 cursor-pointer"><!-- Span que nos permite contener los iconos para visualizar y ocultar las contraseñas al dar click -->
+                <fa v-if="tipoInput == 'password'" icon="eye" class="text-gray-600 w-5 h-5" /><!-- Icono de ojo para visualizar la contraseña -->
+                <fa v-else  icon="eye-slash" class="text-gray-600 w-5 h-5" /><!-- Icono de ojo para ocultar la contraseña -->
               </span>
           </div>
           <button class="btn mt-12" @click="login()">Iniciar Sesión</button>
@@ -29,53 +29,49 @@
 <Spinner class="-mt-1" :modalLoading="modalLoading"></Spinner>
 </template>
 <script>
-const API = process.env.VUE_APP_URL_API_PRODUCCION//constante global que 
-import Footer from "../components/Footer-login.vue";
-import axios from "axios";
-import serviceToken from '../Servicios/Token-Services'
-import { ref } from 'vue'
-import router from '../router';
-import Spinner from "../components/Spinner.vue"
+const API = process.env.VUE_APP_URL_API_PRODUCCION//constante global que  contiene la cadena de conexión al API
+import Footer from "../components/Footer-login.vue";//Importamos el componente Footer que es exclusivo para el login
+import axios from "axios";//Importamos axios para poder hacer peticiones http al API
+import serviceToken from '../Servicios/Token-Services'//Importamos el Servicio que nos va a permitir obtener, decodificar y utilizar el token
+import { ref } from 'vue'//Importmos ref que nos permite que se devuelba un objeto reactivo y mutable, accedemos al valor ocupando .value
+import router from '../router';//Importamos el router de vue para poder navejar hacia rutas existentes
+import Spinner from "../components/Spinner.vue"//Importamos el componente de la pantalla de carga
 export default {
-
   components: {
     Footer,
     Spinner
   },
-  props: {
-    msg: String,
-  },
-  setup(){
+  setup(){//Hook que se encarga de ser el constructor del componente
     const user = ref('')//Constante que almacena el valor del usuario insertado en el input
     const pass = ref('')//Constante que almacena el valor del password insetado en el input
     const message = ref('')//Constante que almacena el posible mensaje de error
-    const modalLoading = ref(false)
-    const tipoInput = ref('password')
+    const modalLoading = ref(false)//Contante que funciona como bandera para activar o desactivar el spinner de pantalla de carga
+    const tipoInput = ref('password')//Constante que indica el tipo de input para mostra u ocultar la contraseña
 
     function login() {//Función que genera el inicio de sesión
       let data = {//Literal que almacena el json que se enviará para iniciar sesión
-        "grant_type": 'password',
+        "grant_type": 'password',//Configuración que permite enviar las credenciales al servidor, si son valida nos regresa el token
         "username": this.user, //parametro que almacena el usuario insertado en el input
         "password": this.pass, //parametro que almacena el password insertado en el input
         "client_id": '', //identificador de publico de la aplicación
         "cliente_secret":''//es una contraseña que generamos con el servidor OAuth
       }
       if(data.username != '' && data.password != ''){//validación para que el username y el password no esté vacio
-        modalLoading.value = true
+        modalLoading.value = true//Activamos la bandera que abre el spinner de la pantalla de carga
         axios.post(`${API}/identity/login`, data) //Se consume el endpoint, se le madna un JSON con los datos necesarios
-        .then((result) => { 
-          message.value = ''
+        .then((result) => { //Si el endpoint tiene una respuesta correcta
+          message.value = ''//Dejamos vacio el mensaje de error
           serviceToken.guardarToken(result.data.access_token) //Guardamos el token utiizando un servicio
           axios.defaults.headers.common['Authorization'] = 'Bearer ' + result.data.access_token //Enviamos el token en la cabecera llamada Authorization porque todos los endpoints lo piden
           router.push('/inicio') //Enviamos la página al incio
-          modalLoading.value = false
-        }).catch((error) => {
-          console.log(error);
-          message.value ="Error, verifica tus datos o intentalo más tarde."
-          modalLoading.value = false
+          modalLoading.value = false//Desactivamos la bandera que abre el spinner de la pantalla de carga
+        }).catch((error) => {//SI el endpoint tiene una respuesta con error
+          console.log(error.request.response);//Mostramos el error en la consola
+          message.value ="Error, verifica tus datos o intentalo más tarde."//Damos un mensaje de error en la interfaz
+          modalLoading.value = false//Desactivamos la bandera que abre el spinner de la pantalla de carga
         })
       }else{//Validación si es que el usuario no inserto algún campo
-        message.value ="Los campos Usuario y COntraseña son obligatorios."
+        message.value ="Los campos Usuario y COntraseña son obligatorios."//Damos un mensaje de error en la interfaz
       }
     }
     return { user, pass, message, modalLoading, tipoInput, login} //Regreasamos las const y las funciones que utilizamos en la vista
