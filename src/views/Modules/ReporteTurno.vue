83a9b60c9206739b8527551fe6b20886bc7ba6ab
DanielD0 <37585619+DanielD0@users.noreply.github.com>
2022-07-25 16:23:50 -0500

    Funcion para descargar 2 reportes

--- src/views/Modules/ReporteTurno.vue
+++ src/views/Modules/ReporteTurno.vue
@@ -4,7 +4,7 @@
 <div class="bg-white w-96 h-96 rounded-lg shadow-md 2xl:w-80 2xl:h-74">
     <h1 class="text-4xl font-bold font-titulo text-center p-2 2xl:text-6xl 2xl:p-4">Reporte Turno</h1>
   <div class="flex w-full justify-center mt-4 gap-8 2xl:gap-20 2xl:mt-10">
-    <div class="flex flex-col gap-14 2xl:gap-20">
+    <div class="flex flex-col gap-14 2xl:gap-14">
         <div>
             <label for="">Plaza de Cobro</label>
         </div>
@@ -14,9 +14,11 @@
         <div>
             <label for="">Fecha</label>
         </div>
-        
+        <div>
+          <label>Tipo de Reporte</label>
+        </div>
     </div>
-    <div class="flex flex-col gap-14 2xl:gap-20">
+    <div class="flex flex-col gap-14 2xl:gap-14">
          <div>
             <label for="">Mazanillo</label>
         </div>
@@ -31,12 +33,20 @@
         <div>
             <input type="date" class="input" v-model="cajero.fecha" :max="hoy">
         </div>
+        <div>
+            <select class="input" v-model="cajero.reporte"  placeholder="XXXXX">
+              <option value="undefined" disabled>Seleccione una opción</option>
+              <option value="1">Concentrado</option>
+              <option value="2">Transacciones</option>
+              <option value="3">Ambos</option>
+            </select>
+      </div>
     </div>
   </div>
-  <div class="flex w-full justify-center p-14 2xl:p-20">
+  <div class="flex w-full justify-center p-14 ">
       <div>
-          <button class="border w-40 bg-ferromex text-white ferromex-color" :class="{'cursor-not-allowed' : bloquearbutton}" @click="generareportever(cajero.turno,cajero.fecha)">Vista previa</button>
-          <button class="border w-40 bg-ferromex text-white ferromex-color" :class="{'cursor-not-allowed' : bloquearbutton}" @click="generareportedescargar(cajero.turno,cajero.fecha)">Descargar Reporte</button>
+          <button class="border w-40 bg-ferromex text-white ferromex-color" :class="{'cursor-not-allowed' : bloquearbutton}" @click="generareportever(cajero.turno,cajero.fecha,cajero.reporte)">Vista previa</button>
+          <button class="border w-40 bg-ferromex text-white ferromex-color" :class="{'cursor-not-allowed' : bloquearbutton}" @click="generareportedescargar(cajero.turno,cajero.fecha,cajero.reporte)">Descargar Reporte</button>
       </div>
   </div>
 </div>
@@ -62,15 +72,18 @@ setup(){
 
   const cajero = reactive({
     turno: undefined,
-    fecha: "" 
+    fecha: "",
+    reporte: undefined 
   })
   const hoy = ref('')
+  const bloquearbutton = ref(false)
   onMounted(()=>{
         hoy.value = new Date().toISOString().split("T")[0];
   })
   const rutapdf = ref('')
-  function generareportever(idturno,fechareporte){
-    if((idturno == undefined && fechareporte == undefined) || idturno == undefined || fechareporte == undefined){
+  function generareportever(idturno,fechareporte,reporte){
+    console.log(reporte)
+    if((idturno == undefined && fechareporte == undefined) || idturno == undefined || fechareporte == undefined || reporte== undefined){
      notify({
             title:'Sin parametros',
             text:'Para generar un reporte debes llenar los parametros necesarios' ,
@@ -78,12 +91,20 @@ setup(){
      });
     }else{
     //Generamos la ruta que hara la llamada a la generacion de los reportes
-    ServiceFiles.xml_hhtp_request(`${API}/Ferromex/Download/pdf/reporteOperativo/reporteTurno/concentrado/${idturno}/${fechareporte}`)
-    ServiceFiles.xml_hhtp_request(`${API}/Ferromex/Download/pdf/reporteOperativo/reporteTurno/transacciones/${idturno}/${fechareporte}`, 'TransaccionesTurno.pdf')
+    if(reporte == "1"){
+      ServiceFiles.xml_hhtp_request(`${API}/Ferromex/Download/pdf/reporteOperativo/reporteTurno/concentrado/${idturno}/${fechareporte}`)
+    }else if(reporte == "2"){
+      ServiceFiles.xml_hhtp_request(`${API}/Ferromex/Download/pdf/reporteOperativo/reporteTurno/transacciones/${idturno}/${fechareporte}`)
+    }else if(reporte == "3"){
+      ServiceFiles.xml_hhtp_request(`${API}/Ferromex/Download/pdf/reporteOperativo/reporteTurno/concentrado/${idturno}/${fechareporte}`)
+      ServiceFiles.xml_hhtp_request(`${API}/Ferromex/Download/pdf/reporteOperativo/reporteTurno/transacciones/${idturno}/${fechareporte}`)
+    }
+    
+  
     
   }
   }
-  function generareportedescargar(idturno,fechareporte){
+  function generareportedescargar(idturno,fechareporte,reporte){
     if((idturno == undefined && fechareporte == undefined) || idturno == undefined || fechareporte == undefined){
      notify({
             title:'Sin parametros',
@@ -92,11 +113,17 @@ setup(){
      });
     }else{
     //Generamos la ruta que hara la llamada a la generacion de los reportes
-    ServiceFiles.descargararchivo(`${API}/Ferromex/Download/pdf/reporteOperativo/reporteTurno/concentrado/${idturno}/${fechareporte}`,fechareporte + ' ConcentradoTurno.pdf')
-    ServiceFiles.descargararchivo(`${API}/Ferromex/Download/pdf/reporteOperativo/reporteTurno/transacciones/${idturno}/${fechareporte}`,fechareporte + ' TransaccionesTurno.pdf')
+    if(reporte == "1"){
+      ServiceFiles.descargararchivo(`${API}/Ferromex/Download/pdf/reporteOperativo/reporteTurno/concentrado/${idturno}/${fechareporte}`,fechareporte + ' ConcentradoTurno.pdf')
+    }else if(reporte == "2"){
+      ServiceFiles.descargararchivo(`${API}/Ferromex/Download/pdf/reporteOperativo/reporteTurno/transacciones/${idturno}/${fechareporte}`,fechareporte + ' TransaccionesTurno.pdf')
+    }else if(reporte == "3"){
+      ServiceFiles.descargararchivo(`${API}/Ferromex/Download/pdf/reporteOperativo/reporteTurno/concentrado/${idturno}/${fechareporte}`,fechareporte + ' ConcentradoTurno.pdf')
+      ServiceFiles.descargararchivo(`${API}/Ferromex/Download/pdf/reporteOperativo/reporteTurno/transacciones/${idturno}/${fechareporte}`,fechareporte + ' TransaccionesTurno.pdf')
+    }
   }
   }
-return {generareportever,generareportedescargar,cajero,hoy,rutapdf}
+return {generareportever,generareportedescargar,cajero,hoy,rutapdf,bloquearbutton}
 }
 }
 </script>
