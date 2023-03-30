fc2dd3509ae410cd4c86160a04bc795e13254b44
Carlos Pulido <cjlpulido@hotmail.com>
2023-03-30 12:02:02 -0600

    V1.4.0

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,7 @@
                 <p class="text-white text-xs hidden md:contents 2xl:text-sm 4k:text-2xl">
                     BOSQUE DE CIRUELOS NO. 99. COL. BOSQUES DE LAS LOMAS, MÉXICO D.F.,C.P 117000
                 </p>
-                <p class="text-white text-xs 2xl:text-sm 4k:text-2xl">V1.0</p>
+                <p class="text-white text-xs 2xl:text-sm 4k:text-2xl">V1.4.0</p>
             </div>                                
         </div>        
     </footer>
